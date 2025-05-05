b928db22199c45ed083ad5357ee5f340e1bc2f3e
giusmili <giusmili67@gmail.com>
2025-05-05 10:55:50 +0200

    php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulaire d'inscription</title>
     <link rel="stylesheet" href="./css/main.css">
