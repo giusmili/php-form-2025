0979ecf618fb0f72a6dbc4281b83c507a1639f70
giusmili <giusmili67@gmail.com>
2025-05-05 17:25:54 +0200

    php

--- controller/controllerForm.php
+++ controller/controllerForm.php
@@ -1,15 +1,12 @@
 <?php
-        $hasEmpty = false;
-               foreach ($_POST as $value) {
-                   if (empty($value)) {
+    $hasEmpty = false;
+        foreach ($_POST as $value) {
+                if (empty($value)) {
                        $hasEmpty = true;
                        break;
-                   }
-               }
-       
-
-
-            # Vérifie s'il y a un champ vide
+                }
+            }
+    # Vérifie s'il y a un champ vide
            
                 
             
